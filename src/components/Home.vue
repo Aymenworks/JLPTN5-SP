5f1c473183c7f6ee468e74c584e72e769c74b89c
Aymen Rebouh <aymenmse@gmail.com>
2018-11-02 11:39:34 +0900

    Add simple grid component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
-    Home
+      <v-container grid-list-lg>
+        <v-layout align-space-between justify-start row wrap>
+          <v-flex v-for="i in 82" xs4>
+             <v-card color="transparent">
+               <v-responsive :aspect-ratio="1/1">
+                 <v-card-text>月</v-card-text>
+                 <v-divider light/>
+                 <v-card-text>Moon</v-card-text>
+               </v-responsive>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
   </div>
 </template>
