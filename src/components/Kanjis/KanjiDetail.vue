da95daf824ea4528ee756b2582954e0797154f03
Aymen Rebouh <aymenmse@gmail.com>
2018-11-08 17:41:44 +0900

    Add kanji detail and use named routes

---
+++ src/components/Kanjis/KanjiDetail.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <v-toolbar light>
+        <v-btn icon @click="goBack" flat><v-icon>arrow_back</v-icon></v-btn>
+    </v-toolbar>
+          
+    <p id="kanjiDetailCharacter" class="text-japanese">{{ kanji.character }}</p>
+    <p id="kanjiDetailTranslation">{{ kanji.translation }}</p>
+  </div>
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            kanji: this.$route.params.kanji
+        }
+    },
+    methods: {
+        goBack() {
+            window.history.length > 1 ? this.$router.go(-1) : 
+                                        this.$router.push({ name: 'home' })
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+#kanjiDetailCharacter {
+    text-align: center;
+    color: black;
+    font-weight: bold;
+    font-size: 150px;
+}
+
+#kanjiDetailTranslation {
+    text-align: center;
+    color: rgb(180, 180, 180);
+    font-weight: bold;
+    font-size: 40px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Home from '@/components/Kanjis/Home'
+import KanjiDetail from '@/components/Kanjis/KanjiDetail'
 import About from '@/components/About/About'
 import Quizzes from '@/components/Quizzes/Quizzes'
 import VueRouter from 'vue-router'
@@ -7,9 +8,10 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [
-  { path: '/', name: 'Home', component: Home },
-  { path: '/about', name: 'About', component: About },
-  { path: '/quizzes', name: 'Quizzes', component: Quizzes}
+  { path: '/', name: 'home', component: Home },
+  { path: '/about', name: 'about', component: About },
+  { path: '/kanjiDetailTest', name: 'kanjiDetail', component: KanjiDetail },
+  { path: '/quizzes', name: 'quizzes', component: Quizzes}
 ]
 
 export default new VueRouter({
