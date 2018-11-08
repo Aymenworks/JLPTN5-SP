229f4596bbb4e6303d53a10bd56fcb962d0fdcec
Aymen Rebouh <aymenmse@gmail.com>
2018-11-08 17:43:09 +0900

    Fix vuejs layout by putting the v-app tag

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
-  <div id="app">
-    <v-card height="200px" flat>
-      
-      <router-view/>
+  <v-app id="app">
+        <v-content>
+          <router-view/>
+        </v-content>
 
-      <v-bottom-nav>
+        <v-bottom-nav>
+          <v-btn color="teal" flat :to="{ name: 'home'}">
+            <span>Home</span>
+            <v-icon>home</v-icon>
+          </v-btn>
 
-        <v-btn color="teal" flat :to="{ name: 'home'}">
-          <span>Home</span>
-          <v-icon>home</v-icon>
-        </v-btn>
+          <v-btn color="teal" flat :to="{ name: 'quizzes'}">
+            <span>Quizzes</span>
+            <v-icon>book</v-icon>
+          </v-btn>
 
-        <v-btn color="teal" flat :to="{ name: 'quizzes'}">
-          <span>Quizzes</span>
-          <v-icon>book</v-icon>
-        </v-btn>
-
-        <v-btn color="teal" flat :to="{ name: 'about'}">
-          <span>About</span>
-          <v-icon>help</v-icon>
-        </v-btn>
-      </v-bottom-nav>
-  </v-card>
-  </div>
+          <v-btn color="teal" flat :to="{ name: 'about'}">
+            <span>About</span>
+            <v-icon>help</v-icon>
+          </v-btn>
+        </v-bottom-nav>
+  </v-app>
 </template>
 
 <style>
