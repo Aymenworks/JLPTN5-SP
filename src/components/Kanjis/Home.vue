7f3a93a291d7ade4bea720930b5495b282379565
Aymen Rebouh <aymenmse@gmail.com>
2018-11-13 17:07:23 +0900

    Add a singleton for the kanji list

--- src/components/Kanjis/Home.vue
+++ src/components/Kanjis/Home.vue
@@ -15,61 +15,31 @@
       </v-menu>
     </v-toolbar>
 
-    <div class="loading" v-if="loading">
-      Loading...
-    </div>
-      <v-container grid-list-lg v-if="kanjis">
-        <v-layout align-space-between justify-start row wrap>
-          <v-flex xs4 v-for="kanji in kanjis" :key="kanji.character">
-             <KanjiItem :kanji="kanji"/>
-          </v-flex>
-        </v-layout>
-      </v-container>
+    <v-container grid-list-lg v-if="kanjis">
+      <v-layout align-space-between justify-start row wrap>
+        <v-flex xs4 v-for="kanji in kanjis" :key="kanji.character">
+            <KanjiItem :kanji="kanji"/>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </div>
 </template>
 
 <script>
 import KanjiItem from '@/components/Kanjis/KanjiItem'
-import KanjiJSON from '../../assets/KanjiEN.json'
-import {Kanji} from './Kanji.js'
-import {Word} from './Word.js'
-import {Pronunciation} from './Pronunciation.js'
+import KanjiManager from './KanjiManager.js'
 
 export default {
   components: {
     KanjiItem
   },
-  created() {
-    this.loading = true
-    this.fetchKanjis()
-    this.loading = false
-  },
   data() {
     return {
-      loading: false,
-      kanjis: null,
+      kanjis: KanjiManager.kanjis,
       sections: [
         "Kanjis", "Hiragana", "Katakana"
       ]
     }
   },
-  methods: {
-    fetchKanjis() {
-        var kanjisList = KanjiJSON[0]['KanjiList']; 
-        var kanjisSymbols = [];
-        for(var i = 0; i< kanjisList.length; i++) {
-          var pronunciation = new Pronunciation(kanjisList[i]['Pronunciation']['Onyomi']['Romaji'], kanjisList[i]['Pronunciation']['Kunyomi']['Romaji']);
-          var words = kanjisList[i]['Words'].map(word => new Word(word['Japanese meaning'], word['Translation meaning'], word['Sound URL']));
-          var newKanji = new Kanji(
-            String(kanjisList[i]['Symbol']), 
-            String(kanjisList[i]['Translation']), 
-            words,
-            pronunciation
-          );
-          kanjisSymbols.push(newKanji);
-        }
-        this.kanjis = kanjisSymbols;
-    }
-  }
 }
 </script>
---
+++ src/components/Kanjis/KanjiManager.js
@@ -0,0 +1,28 @@
+import KanjiJSON from '../../assets/KanjiEN.json'
+import {Kanji} from './Kanji.js'
+import {Word} from './Word.js'
+import {Pronunciation} from './Pronunciation.js'
+
+class KanjiManager {
+  constructor () {
+    var kanjisList = KanjiJSON[0]['KanjiList']
+    var kanjisSymbols = []
+    for (var i = 0; i < kanjisList.length; i++) {
+      var pronunciation = new Pronunciation(kanjisList[i]['Pronunciation']['Onyomi']['Romaji'], kanjisList[i]['Pronunciation']['Kunyomi']['Romaji'])
+      var words = kanjisList[i]['Words'].map(word => new Word(word['Japanese meaning'], word['Translation meaning'], word['Sound URL']))
+      var newKanji = new Kanji(
+        String(kanjisList[i]['Symbol']),
+        String(kanjisList[i]['Translation']),
+        words,
+        pronunciation
+      )
+      kanjisSymbols.push(newKanji)
+    }
+    this.kanjis = kanjisSymbols
+  }
+}
+
+const instance = new KanjiManager()
+Object.freeze(instance)
+
+export default instance
